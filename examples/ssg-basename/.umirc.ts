7702c997b00fee3c982b878d4846dc21c3a12378
Marco. JR <mornsophannamis@gmail.com>
2024-09-28 09:38:41 +0700

    remove unused code

--- examples/ssg-basename/.umirc.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  exportStatic: {},
-  ssr: {},
-  base: '/base/',
-  publicPath: '/base/', // 布署时需要布署在 base 文件夹下.
-};
--- examples/ssr-basename/.umirc.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  ssr: {},
-  exportStatic: {},
-  base: '/base/',
-};
